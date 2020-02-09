b30fc97952ccb52b98291feeb390f9bfbc5c1f35
camileGhastine <camileghastine@hotmail.com>
2020-02-09 18:02:18 +0100

    start design front

---
+++ CamileApp/Core/Constraints/ValidationForm/stdValidationForm.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace CamileApp\Core\Constraints\ValidationForm;
+
+
+class stdValidationForm extends ValidationForm
+{
+    protected $name;
+    protected $description;
+    protected $pass;
+    protected $passConfirm;
+    protected $oldPass;
+    protected $first_name;
+    protected $last_name;
+    protected $email;
+    protected $subject;
+    protected $content;
+    protected $title;
+    protected $chapo;
+    protected $pseudo;
+    protected $pass;
+    protected $statut;
+    protected $keys;
+
+    public function __construct($keys)
+    {
+        $this->keys = $keys;
+    }
+
+}
